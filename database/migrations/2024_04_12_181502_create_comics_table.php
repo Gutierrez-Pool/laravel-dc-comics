bb51959c9fba925a256671a24f08e168e33fad61
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-04-14 13:32:42 -0500

    seeder eseguito

--- database/migrations/2024_04_12_181502_create_comics_table.php
+++ database/migrations/2024_04_12_181502_create_comics_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->string('series', 100);
             $table->date('sale_date');
             $table->string('type', 100);
-            $table->text('artists');
-            $table->text('writers');
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
 
 
             $table->timestamps();
